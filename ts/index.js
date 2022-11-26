7da1fccd8ba3f4feb05eff9684b4f48196c35770
PT <20013003@mail.ru>
2022-11-27 02:11:24 +0300

    Implementation of adding new tasks. Styling tasks

--- ts/index.js
+++ ts/index.js
@@ -1,4 +1,37 @@
-var a = "Hello";
-console.log(a);
+let test1 = 'Evaluate the addition and deletion of user IDs.'
+let test2 = 'Pending'
+let test3 = 'Minor'
 
 
+
+const buttonAddTask = document.querySelector('.add-new-task')
+
+buttonAddTask.addEventListener('click', () => {
+	let addTask = document.createElement("div")
+	addTask.innerHTML =
+
+		`<div class="task">
+	        <div class="task__name-new"></div>
+	        <div class="task__status-new"></div>
+	        <div class="task__priority-new"></div>
+	        <button class="change__task"></button>
+		</div>`
+
+	let myNewTask = document.querySelector('.completed')
+	let positionNewTask = myNewTask.parentNode
+	positionNewTask.insertBefore(addTask, myNewTask)
+
+	const positionSelect = document.querySelector('#position')
+
+	const taskName = document.querySelector('.task__name-new')
+	const taskStatus = document.querySelector('.task__status-new')
+	const taskPriority = document.querySelector('.task__priority-new')
+
+	taskName.textContent = test1
+	taskStatus.textContent = test2
+	taskPriority.textContent = test3
+
+	const oldTaskName = document.querySelector('.task__name-new').className = 'task__name'
+	const oldTaskStatus = document.querySelector('.task__status-new').className = 'task__status'
+	const oldTaskPriority = document.querySelector('.task__priority-new').className = 'task__priority'
+})
