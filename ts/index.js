693ffac9105a9dc0d0fb50bdf31adb19c15151a5
PT <20013003@mail.ru>
2022-11-27 11:32:33 +0300

    render components in ts

--- ts/index.js
+++ ts/index.js
@@ -1,37 +1,34 @@
-let test1 = 'Evaluate the addition and deletion of user IDs.'
-let test2 = 'Pending'
-let test3 = 'Minor'
-
-
-
-const buttonAddTask = document.querySelector('.add-new-task')
-
-buttonAddTask.addEventListener('click', () => {
-	let addTask = document.createElement("div")
-	addTask.innerHTML =
-
-		`<div class="task">
-	        <div class="task__name-new"></div>
-	        <div class="task__status-new"></div>
-	        <div class="task__priority-new"></div>
-	        <button class="change__task"></button>
-		</div>`
-
-	let myNewTask = document.querySelector('.completed')
-	let positionNewTask = myNewTask.parentNode
-	positionNewTask.insertBefore(addTask, myNewTask)
-
-	const positionSelect = document.querySelector('#position')
-
-	const taskName = document.querySelector('.task__name-new')
-	const taskStatus = document.querySelector('.task__status-new')
-	const taskPriority = document.querySelector('.task__priority-new')
-
-	taskName.textContent = test1
-	taskStatus.textContent = test2
-	taskPriority.textContent = test3
-
-	const oldTaskName = document.querySelector('.task__name-new').className = 'task__name'
-	const oldTaskStatus = document.querySelector('.task__status-new').className = 'task__status'
-	const oldTaskPriority = document.querySelector('.task__priority-new').className = 'task__priority'
-})
+var render = function () {
+    var test1 = 'Evaluate the addition and deletion of user IDs.';
+    var test2 = 'Pending';
+    var test3 = 'Minor';
+    var buttonAddTask = document.querySelector('.add-new-task');
+    buttonAddTask === null || buttonAddTask === void 0 ? void 0 : buttonAddTask.addEventListener('click', function () {
+        var addTask = document.createElement("div");
+        addTask.innerHTML =
+            "<div class=\"task\">\n\t        <div class=\"task__name-new\"></div>\n\t        <div class=\"task__status-new\"></div>\n\t        <div class=\"task__priority-new\"></div>\n\t        <button class=\"change__task\"></button>\n\t\t</div>";
+        var myNewTask = document.querySelector('.completed');
+        var positionNewTask = myNewTask === null || myNewTask === void 0 ? void 0 : myNewTask.parentNode;
+        positionNewTask === null || positionNewTask === void 0 ? void 0 : positionNewTask.insertBefore(addTask, myNewTask);
+        var taskName = document.querySelector('.task__name-new');
+        var taskStatus = document.querySelector('.task__status-new');
+        var taskPriority = document.querySelector('.task__priority-new');
+        if (!taskName || !taskStatus || !taskPriority) {
+            console.log('We have not this element');
+            return;
+        }
+        taskName.textContent = test1;
+        taskStatus.textContent = test2;
+        taskPriority.textContent = test3;
+        taskName.className = 'task__name';
+        taskStatus.className = 'task__status';
+        taskPriority.className = 'task__priority';
+    });
+};
+var renderInput = function () {
+    var findId = document.querySelector('.search');
+    findId.innerHTML =
+        "\n        \t<div class=\"search-icon\"></div>\n\n        \t<input type=\"text\" class=\"search-input\" placeholder=\"Search for any training you want\">\n \t \n\t\t";
+};
+render();
+renderInput();
--- ts/index.ts
+++ ts/index.ts
@@ -1,3 +1,60 @@
-const a: string = "Hello World";
+const render = () => {
+	let test1 = 'Evaluate the addition and deletion of user IDs.'
+	let test2 = 'Pending'
+	let test3 = 'Minor'
 
-console.log(a);
+	const buttonAddTask = document.querySelector('.add-new-task')
+
+	buttonAddTask?.addEventListener('click', () => {
+		let addTask = document.createElement("div")
+		addTask.innerHTML =
+
+			`<div class="task">
+	        <div class="task__name-new"></div>
+	        <div class="task__status-new"></div>
+	        <div class="task__priority-new"></div>
+	        <button class="change__task"></button>
+		</div>`
+
+		let myNewTask = document.querySelector('.completed')
+		let positionNewTask = myNewTask?.parentNode
+
+		positionNewTask?.insertBefore(addTask, myNewTask)
+
+		const taskName = document.querySelector('.task__name-new')
+		const taskStatus = document.querySelector('.task__status-new')
+		const taskPriority = document.querySelector('.task__priority-new')
+
+		if (!taskName || !taskStatus || !taskPriority) {
+			console.log('We have not this element')
+			return
+		}
+		taskName.textContent = test1
+		taskStatus.textContent = test2
+		taskPriority.textContent = test3
+
+		taskName.className = 'task__name'
+		taskStatus.className = 'task__status'
+		taskPriority.className = 'task__priority'
+	})
+}
+
+
+
+
+const renderInput = () => {
+
+	const findId = document.querySelector('.search')
+
+	findId!!.innerHTML =
+		`
+        	<div class="search-icon"></div>
+
+        	<input type="text" class="search-input" placeholder="Search for any training you want">
+ 	 
+		`
+
+}
+
+render()
+renderInput()
